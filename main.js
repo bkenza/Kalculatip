813801823a3c1729773fc2d8c7d09f4041227f5a
Kenza <kenza.boulisfane@gmail.com>
2019-11-21 15:12:56 -0500

    fix validation + theme toggling

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 // Calculate tip
-function calculateTip() {
+function calculateTip () {
     const billAmount = document.getElementById('bill-amount').value;
     const serviceQuality = document.getElementById('service-quality').value;
     let numPeople = document.getElementById('people-num').value;
 
     // validate input
-    if(billAmount ==0) {
+    if (billAmount == 0) {
         alert("Wow, you're so funny!");
         return;
     }
-    else if(billAmount ==="" || serviceQuality==0){
+    else if (billAmount === "" || serviceQuality == 0) {
         alert("Please enter a valid amount!");
         return;
     }
 
     //check whether or not to display "each"
-  if (numPeople === "" || numPeople <= 1) {
-    numPeople = 1;
-    document.getElementById("each").style.display = "none";
-  } else {
-    document.getElementById("each").style.display = "block";
-  }
+    if (numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
 
     //calculations
     var result = (billAmount * serviceQuality) / numPeople;
-    
+
     //round to 2 decimal places
     result = Math.round(result * 100) / 100;
     result = result.toFixed(2);
@@ -34,5 +34,13 @@ function calculateTip() {
     document.getElementById('tip').innerHTML = result;
 }
 
+//toggle theme
+function changeTheme () {
+    document.body.classList.toggle('light-theme');
+}
+
 const calculateButton = document.querySelector('.calc');
 calculateButton.addEventListener('click', calculateTip);
+
+const changeThemeButton = document.getElementById('change-theme-btn');
+changeThemeButton.addEventListener('click', changeTheme)
